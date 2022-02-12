262f68b8c93e4714ff2e21626539f4aded490901
epicalex4444 <alexroe@email.com>
2022-02-13 03:48:20 +1030

    made kernel and bootloader share pointers properly

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -14,67 +14,31 @@ use efi_handover::gop_functions;
 use efi_bindings::EFI_MEMORY_DESCRIPTOR;
 
 #[no_mangle]
-pub extern "C" fn _start(boot_info: efi_bindings::BootInfo) -> u64 {
-    handle_boot_handover(&boot_info);
-    print::print("Framebuffer width: ");
-    print::print("\n");
-    print::print("Memory map size: ");
-    print::print_dec(boot_info.memory_map_size as u32);
-    print::print("\nDescriptor size: ");
-    print::print_dec(boot_info.memory_map_descriptor_size as u32);
-    print::print("\nCount: ");
-    print::print_dec((boot_info.memory_map_size / boot_info.memory_map_descriptor_size) as u32);
-    unsafe{
-        let mut res = 0;
-        let mut oem = 0;
-        for i in 0..(boot_info.memory_map_size / boot_info.memory_map_descriptor_size){
-            
-            let descriptor: *const EFI_MEMORY_DESCRIPTOR = (&boot_info.memory_map as *const EFI_MEMORY_DESCRIPTOR).offset((i * boot_info.memory_map_descriptor_size) as isize);
-            if (*descriptor).t <= 14{
-                print::print("\n");
-                print::print_dec(i.try_into().unwrap());
-                print::print(": ");
-                print::print_dec((*descriptor).t);
-            }
-            else if (*descriptor).t <= 0x6FFFFFFF {
-                res += 1;
-            }
-            else{
-                oem += 1;
-            }
-        }
+pub extern "C" fn _start(boot_info: *const efi_bindings::BootInfo) -> u64 {
+    handle_boot_handover(boot_info);
 
-        print::print("\n Reserved descriptors: ");
-        print::print_dec(res);
-        print::print("\n OEM Descriptors: ");
-        print::print_dec(oem);
-
-        print::print("\nMmap location: ");
-        print::print_hex(((&boot_info.memory_map) as *const efi_bindings::EFI_MEMORY_DESCRIPTOR) as u32);
+    unsafe {
+        return (*boot_info).memory_map as u64;
     }
-   
-    paging::init_paging(&boot_info.memory_map, boot_info.memory_map_size, boot_info.memory_map_descriptor_size);
-
-    return boot_info.memory_map.physical_start as u64;
 }
 
 // Handles the absolutely neccesary setup before anything else can be done.
 fn handle_boot_handover(boot_info: *const efi_bindings::BootInfo) -> () {
     unsafe {
-        gop_functions::gop_init(&(*boot_info).framebuffer);                
+        gop_functions::gop_init((*boot_info).framebuffer);                
         // Set backroundd to black
-        gop_functions::plot_rect(
-            0,
-            0,
-            (*boot_info).framebuffer.width,
-            (*boot_info).framebuffer.height,
-            0,
-            0,
-            0,
-            &(*boot_info).framebuffer,
-        );
+        //gop_functions::plot_rect(
+        //    0,
+        //    0,
+        //    (*(*boot_info).framebuffer).width,
+        //    (*(*boot_info).framebuffer).height,
+        //    0,
+        //    0,
+        //    0,
+        //    (*boot_info).framebuffer,
+        //);
         
-        print::init_print((*boot_info).glyphbuffer, &(*boot_info).framebuffer, true);
+        print::init_print((*boot_info).glyphbuffer, (*boot_info).framebuffer, true);
 
     }
 }
