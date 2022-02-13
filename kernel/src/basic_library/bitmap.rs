2c6e6f37fd3508b5c0873ca5fdfc1cb618b9b811
epicalex4444 <alexroe@email.com>
2022-02-14 05:36:02 +1030

    fixed formatting

--- kernel/src/basic_library/bitmap.rs
+++ kernel/src/basic_library/bitmap.rs
@@ -1,31 +1,29 @@
-pub struct Bitmap{
+pub struct Bitmap {
     pub length: u64,
     pub bitmap_ptr: *mut u8,
 }
 
-impl Bitmap{
-    pub fn new(start_addr: *mut u8, size: u64) -> Bitmap{
-        unsafe{
-            //let mut bitmap: *mut u8 = start_addr;
+impl Bitmap {
+    pub fn new(start_addr: *mut u8, size: u64) -> Bitmap {
+        unsafe {
             for i in 0..size {
                 *start_addr.offset(i as isize) = 0x00;
             }
-            Bitmap{ length:size, bitmap_ptr: start_addr}
+            Bitmap {length: size, bitmap_ptr: start_addr}
         }        
     }
 
-    unsafe fn byte_from_index(&self, index: u64) -> *mut u8{
+    unsafe fn byte_from_index(&self, index: u64) -> *mut u8 {
         self.bitmap_ptr.offset((index / 8) as isize)
     }
 
     // Gets the state of the bit at index
     pub fn get_bit(&self, index: u64) -> bool {
         if index < self.length * 8 {
-            unsafe{
+            unsafe {
                 let byte = self.byte_from_index(index);
                 return *byte & 1 << index % 8 > 0; 
             }
-            //return (self.bits & 1 << index) > 0;
         } else {
             panic!("Index out of bounds of bitmap");
         }
@@ -34,10 +32,9 @@ impl Bitmap{
     // Attempts to set the bit at index, returns false if the index was out of bounds of the bitmap
     pub fn set_bit(&mut self, index: u64) -> bool {
         if index < self.length * 8 {
-            unsafe{
+            unsafe {
                 *(self.byte_from_index(index)) = *(self.byte_from_index(index)) | 1 << index%8;
             }            
-            //self.bits = self.bits | 1 << index;
             return true;
         } else {
             return false;
@@ -50,7 +47,6 @@ impl Bitmap{
             unsafe{
                 *(self.byte_from_index(index)) = *(self.byte_from_index(index)) & !(1 << index%8);
             }            
-            //self.bits = self.bits | 1 << index;
             return true;
         } else {
             return false;
@@ -59,10 +55,11 @@ impl Bitmap{
 
     // Get's the byte which contains the given index, useful for printing
     pub fn get_printable_byte(&mut self, index: u64) -> u8 {
-        if index < self.length * 8{
-            unsafe { return *(self.bitmap_ptr.offset((index / 8) as isize))}
-        }
-        else{
+        if index < self.length * 8 {
+            unsafe {
+                return *(self.bitmap_ptr.offset((index / 8) as isize))
+            }
+        } else {
             panic!("Index outside bounds of the array");
         }
     }
