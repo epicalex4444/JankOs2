635fdd10c21d2e86fa82ab8ac2e4e9bb3e189e79
epicalex4444 <alexroe@email.com>
2022-02-24 01:42:58 +1030

    tons of functions that can only be done with inline assembly

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -8,6 +8,7 @@
 #![allow(dead_code)]
 
 mod bitmap;
+mod asm;
 mod efi_bindings;
 mod gop_functions;
 mod math;
@@ -21,10 +22,6 @@ use gop_functions::{
     gop_init,
     clear_screen
 };
-use paging::{
-    request_page,
-    init_paging
-};
 
 #[no_mangle]
 pub extern "C" fn _start(boot_info: *const BootInfo) -> u64 {
@@ -33,19 +30,44 @@ pub extern "C" fn _start(boot_info: *const BootInfo) -> u64 {
         Writer::init((*boot_info).glyphbuffer, (*boot_info).framebuffer, false);
         clear_screen();
 
-        if init_paging((*boot_info).memory_map, (*boot_info).memory_map_size, (*boot_info).descriptor_size) {
-            panic!("failed to init paging");
-        }
+        println!("Hello, World!");
 
-        let address: u64 = request_page();
-        println!("requested address = {:#X}", address);
+        //let memory_map_entries: u64 = (*boot_info).memory_map_size / (*boot_info).descriptor_size;
+        //for i in 0..memory_map_entries {
+        //    let descriptor: *const EFI_MEMORY_DESCRIPTOR = ((*boot_info).memory_map as u64 + i * (*boot_info).descriptor_size) as *const EFI_MEMORY_DESCRIPTOR;
+        //    println!("physical {:#x}, virtual {:#x}", (*descriptor).physical_start, (*descriptor).virtual_start);
+        //}
 
-        loop {};
+        println!("cr0 = {:#x}", asm::read_cr0());
+        println!("cr2 = {:#x}", asm::read_cr2());
+        println!("cr3 = {:#x}", asm::read_cr3());
+        println!("cr4 = {:#x}", asm::read_cr4());
+        println!("cr8 = {:#x}", asm::read_cr8());
+        println!("efer = {:#x}", asm::read_efer());
+
+        let mut eax:u32 = 0x80000000u32;
+        let mut ebx:u32 = 0;
+        let mut ecx:u32 = 0;
+        let mut edx:u32 = 0;
+        asm::cpuid(&mut eax, &mut ebx, &mut ecx, &mut edx);
+        println!("cpuid:");
+        println!("eax = {:#x}", eax);
+        println!("ebx = {:#x}", ebx);
+        println!("ecx = {:#x}", ecx);
+        println!("edx = {:#x}", edx);
+
+        println!("GoodBye, World!");
+
+        loop {
+            asm::hlt();
+        };
     }
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     println!("{}", _info);
-    loop {}
+    loop {
+        asm::hlt();
+    }
 }
